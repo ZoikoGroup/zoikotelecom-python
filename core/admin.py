c01c62db1cc8abc2ec8b001470f3fb3178212ed4
Anowar Sadat <anowar.vista@gmail.com>
2026-05-19 17:57:23 +0530

    Updated2

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-admin.site.site_header = "Zoiko Telecom Admin"        # Header at the top of admin
-admin.site.site_title = "Zoiko Telecom Admin Portal"  # Browser tab title
+admin.site.site_header = "ZoikoTelecom Admin"        # Header at the top of admin
+admin.site.site_title = "ZoikoTelecom Admin Portal"  # Browser tab title
 admin.site.index_title = "Dashboard"          # Index page title
