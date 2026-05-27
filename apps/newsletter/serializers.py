d5996fd599f5ccbc20ad75127c931a915909bf81
Anowar Sadat <anowar.vista@gmail.com>
2026-05-27 19:53:18 +0530

    Updated31

--- apps/newsletter/serializers.py
+++ apps/newsletter/serializers.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
 from .models import Subscriber
 
-
 class SubscriberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscriber
         fields = ['id', 'email', 'subscribed_at']
         
-
     def validate_email(self, value):
         if Subscriber.objects.filter(email=value).exists():
             raise serializers.ValidationError("This email is already subscribed.")
